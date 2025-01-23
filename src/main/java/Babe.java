f3671409ac1e43ac18cdb584f0a6f093ec277140
avinazz3 <125176749+avinazz3@users.noreply.github.com>
2025-01-23 16:16:21 +0800

    Echo

--- src/main/java/Babe.java
+++ src/main/java/Babe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Babe {
     public static void main(String[] args) {
         String logo = " ____        _          \n"
@@ -6,14 +8,23 @@ public class Babe {
                 + "| |_) | (_| | |_) |  __/\n"
                 + "|____/ \\__,_|_.__/ \\___|\n";
         System.out.println("Hello from\n" + logo);
-
         printGreet();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+        do {
+            input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        } while (!input.equals("bye"));
+
         printExit();
     }
 
     private static void printGreet() {
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Babe");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
     }
