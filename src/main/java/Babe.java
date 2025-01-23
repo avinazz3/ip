69401542cdff1222dee917f1e6cf8c89c59f88b3
avinazz3 <125176749+avinazz3@users.noreply.github.com>
2025-01-23 16:37:49 +0800

    Mark as done

--- src/main/java/Babe.java
+++ src/main/java/Babe.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Babe {
     public static void main(String[] args) {
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
         String logo = """
                  ____        _         \s
                 | __ )  __ _| |__   ___\s
@@ -10,10 +13,8 @@ public class Babe {
                 |____/ \\__,_|_.__/ \\___|
                 """;
         System.out.println("Hello from\n" + logo);
-        printGreet();
 
-        String[] tasks = new String[100];
-        int taskCount = 0;
+        printGreet();
         Scanner scanner = new Scanner(System.in);
 
         String input;
@@ -22,11 +23,22 @@ public class Babe {
             System.out.println("    ____________________________________________________________");
 
             if (input.equals("list")) {
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                    System.out.println("     " + (i + 1) + "." + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       " + tasks[index]);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println("     OK, I've marked this task as not done yet:");
+                System.out.println("       " + tasks[index]);
             } else if (!input.equals("bye")) {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("     added: " + input);
             }
@@ -38,16 +50,18 @@ public class Babe {
         printExit();
     }
 
-
     private static void printGreet() {
-        System.out.println("____________________________________________________________");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Babe");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
     }
 
     private static void printExit() {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
     }
 }
